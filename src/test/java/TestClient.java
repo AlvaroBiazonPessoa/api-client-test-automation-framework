6d5254e4697728990489b3ee7419b43df3e1a99b
AlvaroBiazonPessoa <alvarobiazon@hotmail.com>
2023-12-12 15:37:31 -0300

    feat: Adding function registerClient

--- src/test/java/TestClient.java
+++ src/test/java/TestClient.java
@@ -3,15 +3,16 @@ import org.hamcrest.core.IsEqual;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.containsString;
 
 public class TestClient {
 
     String clientApiAddress = "http://localhost:8080/";
+    String endpoitClient = "cliente";
 
     @Test
     @DisplayName("When get all clients then list should be empty")
     public void getAllClients() {
-
         String expectedResponse = "{}";
 
         given()
@@ -21,6 +22,26 @@ public class TestClient {
         .then()
                 .statusCode(200)
                 .assertThat().body(new IsEqual<>(expectedResponse));
+    }
+
+    @Test
+    @DisplayName("When registering a client, it must be available in the results")
+    public void registerClient() {
+        String clienteToRegister = "{\n" +
+                "  \"id\": 3,\n" +
+                "  \"idade\": 24,\n" +
+                "  \"nome\": \"Beatriz\",\n" +
+                "  \"risco\": 10\n" +
+                "}";
+        String bodyOfExpectedResponse = "{\"3\":{\"nome\":\"Beatriz\",\"idade\":24,\"id\":3,\"risco\":10}}";
+        given()
+                .contentType(ContentType.JSON)
+                .body(clienteToRegister)
+        .when()
+                .post(clientApiAddress+endpoitClient)
+        .then()
+                .statusCode(201)
+                .assertThat().body(containsString(bodyOfExpectedResponse));
     }
 
 }
