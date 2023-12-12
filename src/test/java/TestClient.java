141e6122a2febf15f7799b6039bedf58bf96fe68
AlvaroBiazonPessoa <alvarobiazon@hotmail.com>
2023-12-11 21:32:19 -0300

    feat: Adding variable clintApiAddres in class TestClient

--- src/test/java/TestClient.java
+++ src/test/java/TestClient.java
@@ -1,7 +1,11 @@
+import io.restassured.http.ContentType;
+import org.hamcrest.core.IsEqual;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import static io.restassured.RestAssured.given;
 
 public class TestClient {
 
+    String clientApiAddress = "http://localhost:8080/";
 
 }
