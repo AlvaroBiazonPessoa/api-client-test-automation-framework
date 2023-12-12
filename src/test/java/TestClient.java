ffc5310ed4eefb9c8c9cf9867abe0b71aabc2657
AlvaroBiazonPessoa <alvarobiazon@hotmail.com>
2023-12-12 16:04:14 -0300

    feat: Adding function updateClient

--- src/test/java/TestClient.java
+++ src/test/java/TestClient.java
@@ -44,4 +44,24 @@ public class TestClient {
                 .assertThat().body(containsString(bodyOfExpectedResponse));
     }
 
+    @Test
+    @DisplayName("When the client is updated, then the new information should appear in the result")
+    public void updateClient() {
+        String clienttoUpdate = "{\n" +
+                "  \"id\": 3,\n" +
+                "  \"idade\": 33,\n" +
+                "  \"nome\": \"Ivan Carneiro\",\n" +
+                "  \"risco\": 1\n" +
+                "}";
+        String bodyOfExpectedResponse = "{\"3\":{\"nome\":\"Ivan Carneiro\",\"idade\":33,\"id\":3,\"risco\":1}}";
+        given()
+                .contentType(ContentType.JSON)
+                .body(clienttoUpdate)
+        .when()
+                .put(clientApiAddress+endpoitClient)
+        .then()
+                .statusCode(200)
+                .assertThat().body(containsString(bodyOfExpectedResponse));
+    }
+
 }
