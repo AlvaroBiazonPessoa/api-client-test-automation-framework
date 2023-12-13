5861105f723c0092d3dd83baa093ac853bdf2015
AlvaroBiazonPessoa <alvarobiazon@hotmail.com>
2023-12-13 16:09:51 -0300

    fix: Removing imports that are not being used

--- src/test/java/TestClient.java
+++ src/test/java/TestClient.java
@@ -1,11 +1,9 @@
 import io.restassured.http.ContentType;
 import io.restassured.response.ValidatableResponse;
 import org.apache.http.HttpStatus;
-import org.hamcrest.core.IsEqual;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import static io.restassured.RestAssured.given;
-import static org.hamcrest.Matchers.containsString;
 import static org.hamcrest.Matchers.equalTo;
 
 public class TestClient {
