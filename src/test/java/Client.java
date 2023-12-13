5fb2d45f0e84b1bc5b93a500d5feb13b9127d9e3
AlvaroBiazonPessoa <alvarobiazon@hotmail.com>
2023-12-12 22:35:22 -0300

    feat: Creating file and class Client

---
+++ src/test/java/Client.java
@@ -0,0 +1,35 @@
+public class Client {
+
+    private String name;
+    private int id, age;
+
+    public Client(String name, int id, int age) {
+        this.name = name;
+        this.id = id;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+}
